786d2b9cd4b947859c83907fcae413eba3fd2024
Jules Hummelink <julescitronic.nexus@gmail.com>
2021-05-22 02:26:06 +0200

    FAB.js 1.1

--- FAB.js
+++ FAB.js
@@ -32,7 +32,7 @@ class FAB{
 
 	#addFab(){
 		this.#fabObject = document.createElement('div');
-		this.#fabObject.style.position = 'absolute';
+		this.#fabObject.style.position = 'fixed';
 		this.#fabObject.style.display = 'flex';
 		this.#fabObject.style.justifyContent = 'center';
 		this.#fabObject.style.alignItems = 'center';
@@ -42,6 +42,7 @@ class FAB{
 		this.#fabObject.style.boxShadow = '4px 4px 8px 0 rgba(0, 0, 0, 0.5)';
 		this.#fabObject.style.transform = 'scale(1)';
 		this.#fabObject.style.transition = '0.2s';
+		this.#fabObject.style.opacity = '0';
 
 		this.#fabObject.style.backgroundColor = this.#color;
 		let fabObject = this.#fabObject;
@@ -54,27 +55,57 @@ class FAB{
 			}, 200);
 		});
 		this.#fabObject.addEventListener('click', this.action);
+		//var viewportOffset = this.#parent.getBoundingClientRect();
+		let parent = this.#parent;
+		let offset = 30;
+		let location = this.#location;
 
+		setInterval(function(){
+			var viewportOffset = parent.getBoundingClientRect();
+			switch(location){
+				case 0:
+					fabObject.style.left = offset + viewportOffset.left + 'px';
+					fabObject.style.bottom = offset + (window.innerHeight - viewportOffset.bottom) + 'px';
+					break;
+				case 1:
+					fabObject.style.right = offset + (window.innerWidth - viewportOffset.right) + 'px';
+					fabObject.style.bottom = offset + (window.innerHeight - viewportOffset.bottom) + 'px';
+					break;
+				case 2:
+					fabObject.style.top = offset + viewportOffset.top + 'px';
+					fabObject.style.left = offset + viewportOffset.left + 'px';
+					break;
+				case 3:
+					fabObject.style.top = offset + viewportOffset.top + 'px';
+					fabObject.style.right = offset + (window.innerWidth - viewportOffset.right) + 'px';
+				default:
+					console.error('FAB Location not specified or invalid...');
+			}
+			fabObject.style.opacity = '1';
+		}, 100);
+
+		/*
 		switch(this.#location){
 			case 0:
-				this.#fabObject.style.left = '50px';
-				this.#fabObject.style.bottom = '50px';
+				this.#fabObject.style.left = offset + viewportOffset.left + 'px';
+				this.#fabObject.style.bottom = offset + viewportOffset.bottom + 'px';
 				break;
 			case 1:
-				this.#fabObject.style.right = '50px';
-				this.#fabObject.style.bottom = '50px';
+				this.#fabObject.style.right = offset + viewportOffset.right + 'px';
+				this.#fabObject.style.bottom = offset + viewportOffset.bottom + 'px';
 				break;
 			case 2:
-				this.#fabObject.style.top = '50px';
-				this.#fabObject.style.left = '50px';
+				this.#fabObject.style.top = offset + viewportOffset.top + 'px';
+				this.#fabObject.style.left = offset + viewportOffset.left + 'px';
 				break;
 			case 3:
-				this.#fabObject.style.top = '50px';
-				this.#fabObject.style.right = '50px';
+				this.#fabObject.style.top = offset + viewportOffset.top + 'px';
+				this.#fabObject.style.right = offset + viewportOffset.right + 'px';
 			default:
 				console.error('FAB Location not specified or invalid...');
 		}
-
+		*/
+		
 		let fabIcon = document.createElement('img');
 		fabIcon.style.width = '30px';
 		fabIcon.style.height = '30px';
